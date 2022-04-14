79c5e3c313005885b7577d2a58b110769a6fd386
JodagonDrago <josephHaloman0@gmail.com>
2022-04-14 16:21:22 -0700

    comments added

--- src/main.js
+++ src/main.js
@@ -15,3 +15,9 @@ let music;
 //set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+
+// Joseph Squires, Dragon Patrol, 4/14/2022, 6 hours to complete
+// 60 points: Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi)
+// 30 points: Implement a simultaneous two-player mode
+// 5 points: Add your own (copyright-free) background music to the Play scene
+// #FACADE: Made rockets randomly increase and decrease their speeds during gameplay (cannot reach 0 or go above 7)
